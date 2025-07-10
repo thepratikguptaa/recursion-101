ee352dd7d955d5565afdbffd3e8107545f49783a
Pratik Gupta <pratikguptamain@gmail.com>
2025-07-11 00:38:11 +0530

    sum of array in recursion and iterative way

---
+++ sum_array_loop.py
@@ -0,0 +1,5 @@
+def sum_iterative(arr):
+    total = 0
+    for num in arr:
+        total += num
+    return total
---
+++ sum_array_recursive.py
@@ -0,0 +1,5 @@
+def sum_recursive(arr):
+    if not arr:
+        return 0
+    
+    return arr[0] + sum_recursive(arr[1:])
