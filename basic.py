ad75137ccb12e3c433bd12f25c6afc43fff5b2cc
Pratik Gupta <pratikguptamain@gmail.com>
2025-07-10 23:35:08 +0530

    basic recursion

--- basic.py
+++ basic.py
@@ -2,7 +2,8 @@ def printNumbers(Lrange, Urange):
     #base case
     if Lrange > Urange:
         return
-    print(Lrange)
+    print(Lrange) #12345
     printNumbers(Lrange + 1, Urange)
+    print(Lrange) #54321
 
 printNumbers(1, 10)
