75b2aadb7999b120b582db039fc5afe23e5761e8
Pratik Gupta <pratikguptamain@gmail.com>
2025-07-12 13:09:34 +0530

    solved phone mapping problem no. 17 on leetcode

--- phone_mapping_prob-17.py
+++ phone_mapping_prob-17.py
@@ -1,3 +1,13 @@
+"""
+Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent.
+
+A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+
+Example:
+
+Input: "23"
+Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+"""
 from typing import List
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
