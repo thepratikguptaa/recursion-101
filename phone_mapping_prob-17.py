0324179ed674a15fbc9e80ee3a42d7715fc8466b
Pratik Gupta <pratikguptamain@gmail.com>
2025-07-12 13:01:55 +0530

    solved phone mapping problem no. 17 on leetcode

---
+++ phone_mapping_prob-17.py
@@ -0,0 +1,22 @@
+from typing import List
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+        phone_map = {
+            '2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'
+        }
+
+        result = []
+
+        def backtrack(index:int, current:str):
+            #base case
+            if index == len(digits):
+                result.append(current)
+                return
+            
+            for char in phone_map[digits[index]]:
+                backtrack(index + 1, current + char)
+
+        backtrack(0, "")
+        return result
